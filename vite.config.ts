08c9be4b0b6233a835071a97f2c9b9fb0c8133a8
liyuan <l18201107096@163.com>
2025-06-01 12:44:59 +0800

    配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from "@vitejs/plugin-legacy"
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  legacy({
+    targets: ["ie>=11"],
+    additionalLegacyPolyfills: ["regenerator-runtime/runtime"], //解决跨域警告
+  })
+  ],
+  base: './', // 用于处理打包后的静态文件丢失问题
   resolve: {
     // 配置别名
     alias: {
