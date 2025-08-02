564d516c5d2648c12bd758b148648fdcd71db8a6
ADMIN <l18201107096@163.com>
2025-08-02 15:19:39 +0800

    首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
+import MeunBar from '@/components/meunBar.vue';
+
 </script>
 
 <template>
-  <router-view></router-view>
+  <div class="layout-container">
+    <!-- <el-container>：外层容器。 当子元素中包含 <el-header> 或 <el-footer> 时，全部子元素会垂直上下排列， 否则会水平左右排列。/ -->
+    <el-container>
+      <el-scrollbar>
+        <!-- <el-aside>：侧边栏容器。 -->
+        <el-aside width="300px">
+          <MeunBar></MeunBar>
+        </el-aside>
+      </el-scrollbar>
+      <el-container>
+        <!-- <el-header>：顶栏容器。 -->
+        <el-header class="el-header">
+          Header
+        </el-header>
+        <!-- <el-main>：主要区域容器。 -->
+        <el-main class="right-mian">
+          <el-scrollbar>
+            <router-view></router-view>
+          </el-scrollbar>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
-
+.layout-container .el-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: 600;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
 </style>
