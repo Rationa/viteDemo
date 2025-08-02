71e41980fdc7dc79bcf8ecbede1521af72776cb9
ADMIN <l18201107096@163.com>
2025-08-02 14:12:33 +0800

    提交

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,21 +1,143 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-import viteImg from '../../../public/vite.svg'
-import vueImg from '@/assets/vue.svg'
+
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+import { ref, isRef, shallowRef, triggerRef } from 'vue';
+
+type M = {
+  name: string
+}
+
+const name = ref<M>({ name: "miku" });
+
+const age = ref<number>(1);
+
+const shallName = shallowRef<M>({ name: "miku2" })
+
+const change = () => {
+  age.value = 11;
+  // shallowRef 只能赋值给value
+  // shallName.value = {
+  //   name: "18"
+  // }
+  // ref和shallowRef一起写 相当于调用了triggerRef()
+  // name.value.name = "sfsf";
+  // shallName.value.name  = "sfsf";
+
+
+  // console.log("---")
+  // name.value.name = "sfsf";
+  // triggerRef 会去收集ref
+  shallName.value.name = "sfsf";
+  triggerRef(shallName)
+}
+
 
 </script>
 
 <template>
   <div>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img :src="viteImg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img :src="vueImg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
+    <p>
+      {{ name }}
+      {{ shallName }}
+    </p>
+
+    <el-button type="primary" @click="change()">Primary</el-button>
+      <el-row class="tac">
+    <el-col :span="12">
+      <h5 class="mb-2">Default colors</h5>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+    <el-col :span="12">
+      <h5 class="mb-2">Custom colors</h5>
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
   </div>
 </template>
 
--- src/pages/table/index.vue
+++ src/pages/table/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import { ref } from "vue";
+const a = ref();
 </script>
 
 <template>
   <div>table</div>
+  <div v-memo="">{{ a }}</div>
 </template>
 
 <style scoped></style>
